7ee2ff0cd2b279e9270742fc617eb87ec9f1a613
MarekL-MarekL <markandmark3@gmail.com>
2024-04-02 13:02:06 +0200

    en första implementation

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("test");
+
+for (let i = 1; i <= 100; i++) {
+	if (i % 4 == 0 && i % 3 == 0) {
+		console.log("Bish-Bosh");
+	} else if (i % 3 == 0) {
+		console.log("Bish");
+	} else if (i % 4 == 0) {
+		console.log("Bosh");
+	} else {
+		console.log(i);
+	}
+}
